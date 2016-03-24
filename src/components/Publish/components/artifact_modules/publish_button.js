835d30d1294af9e392d0517897e23f2cf7a08d84
OstlerDev <ostlerdev@me.com>
2016-03-24 16:15:06 -0600

    Implement Publish Button

---
+++ src/components/Publish/components/artifact_modules/publish_button.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+export
+default React.createClass({
+    render() {
+        return (
+            <div className="publish-section publish-submit">
+                <div className="row">
+                    <div className="col-sm-9">
+                        <p className="small">{this.props.desc}</p>
+                    </div>
+                    <div className="col-sm-3 text-right">
+                        <button onclick={this.props.onClick} className="btn btn-primary">Publish</button>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+});
--- src/components/Publish/components/type_tabs/music.js
+++ src/components/Publish/components/type_tabs/music.js
@@ -3,10 +3,14 @@ import React from 'react';
 import MetaInfo from '../artifact_modules/meta_information';
 import Pricing from '../artifact_modules/pricing';
 import Table from '../artifact_modules/table';
-import Extra from '../drop_zones/extra';
+import PublishButton from '../artifact_modules/publish_button';
 
 export
 default React.createClass({
+    onPublish(event){
+        // Add link to publisher API here.
+        console.log("Publish Button Clicked");
+    },
     render() {
         return (
             <div className="publish-section information">
@@ -15,6 +19,7 @@ default React.createClass({
                     <Pricing music={true} type={"Song"} desc={"Pick the price for your track and album, lorem ipsum dolor sit amet, consectetur adipisicing elit. Reprehenderit distinctio doloribus maiores ducimus."} />
                     <Table files={[]} type={"audio"} />
                     <Table files={[]} type={"extra"} />
+                    <PublishButton onClick={this.onPublish} desc={"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsam recusandae quae minima accusantium, tempora, sunt quia."} />
                 </div>
             </div>
         );
